614a243a1ee6f967e113510f91272a773377148e
samath-hegde <samathhegde11@gmail.com>
2022-02-14 00:50:27 +0530

    hi this is user 1 first commit

---
+++ src/test/java/demoGit/DemoGitss.java
@@ -0,0 +1,13 @@
+package demoGit;
+
+import org.testng.annotations.Test;
+
+public class DemoGitss {
+	
+	@Test
+	public void demoGit()
+	{
+		System.out.println("hi this is user 1 First commit");
+	}
+
+}
